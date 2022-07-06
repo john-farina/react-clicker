556340bd51be1f44c2c4b93f758aa4d87019a8e8
onefootfarina <clichecontent@gmail.com>
2022-07-05 22:32:30 -0400

    nice

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import './index.css';
 
-function ClickCounter({ count }) {
-    return <h1 className="mainCounter">{count}</h1>;
+function ClickCounter({ count, style }) {
+    return (
+        <h1 style={style} className="mainCounter">
+            {count}
+        </h1>
+    );
 }
 function MainButton({ onClick }) {
     return (
@@ -15,6 +19,34 @@ function MainButton({ onClick }) {
 function Game() {
     const [count, changeCount] = useState(0);
     const [level, changeLevel] = useState(0);
+    const [storeStyle, changeStoreStyle] = useState({
+        display: 'none',
+        backgroundColor: 'red',
+        color: 'blue',
+    });
+    const [storeScreenOpen, changeScreenOpen] = useState(false);
+
+    console.log(storeStyle.style);
+    const hideOrShow = (hideShow) => {
+        if (hideShow === 'hide') {
+            changeStoreStyle({ display: 'none' });
+        } else if (hideShow === 'show') {
+            changeStoreStyle({ display: 'flex' });
+        } else {
+            console.log('ERROR');
+            return;
+        }
+    };
+
+    const storeButtonLogic = () => {
+        if (storeScreenOpen === false) {
+            changeScreenOpen(true);
+            hideOrShow('show');
+        } else if (storeScreenOpen === true) {
+            changeScreenOpen(false);
+            hideOrShow('hide');
+        }
+    };
 
     const clickMath = () => {
         if (level === 0) {
@@ -24,15 +56,18 @@ function Game() {
 
     return (
         <>
-            <ClickCounter count={count} />
-            <MainButton onClick={clickMath} />
-            <button
-                onClick={() => {
-                    changeLevel(level + 1);
-                }}
-            >
-                LEVEL
-            </button>
+            <div id="gameContainer">
+                <ClickCounter style={storeStyle} count={count} />
+                <MainButton onClick={clickMath} />
+                <button
+                    onClick={() => {
+                        changeLevel(level + 1);
+                    }}
+                >
+                    LEVEL
+                </button>
+                <button onClick={storeButtonLogic}>STORE</button>
+            </div>
         </>
     );
 }
