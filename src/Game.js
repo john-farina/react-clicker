1258f52c73387ef7d21060f6e2e2e8a328686cd9
onefootfarina <clichecontent@gmail.com>
2022-07-05 21:47:36 -0400

    beginning test last test

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,29 @@
-import './Game.css';
+import { useState } from 'react';
+import './game.css';
+
+function ClickCounter({ count }) {
+    return <h1>{count}</h1>;
+}
+function MainButton({ onClick }) {
+    return <button onClick={onClick}>CLICK ME</button>;
+}
 
 function Game() {
-    return <h1>hello</h1>;
+    const [count, changeCount] = useState(0);
+    const [level, changeLevel] = useState(0);
+
+    const clickMath = () => {
+        if (level === 0) {
+            changeCount(count + 1);
+        }
+    };
+
+    return (
+        <>
+            <ClickCounter count={count} />
+            <MainButton onClick={clickMath} />
+        </>
+    );
 }
 
 export default Game;
